51c3645f5c95a8e03937c1f7fc2deae19634a76e
korsb <korsbacken@gmail.com>
2024-04-09 15:59:24 +0200

    Initial commit

---
+++ src/main/java/com/humbo/testautomation/HumboTestAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.humbo.testautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumboTestAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumboTestAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/humbo/testautomation/HumboTestAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humbo.testautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumboTestAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
