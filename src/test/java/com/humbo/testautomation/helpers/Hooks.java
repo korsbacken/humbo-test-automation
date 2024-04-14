531f42082f024a23193010a9b0e69f5d25cf491d
korsb <korsbacken@gmail.com>
2024-04-14 21:05:38 +0200

    test

--- src/test/java/com/humbo/testautomation/helpers/Hooks.java
+++ src/test/java/com/humbo/testautomation/helpers/Hooks.java
@@ -18,10 +18,10 @@ public class Hooks {
 //        options.addArguments("--start-maximized");
 //        options.addArguments("--incognito");
         options.addArguments("--headless");
-//        options.addArguments("--window-size=1920,1080");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
 
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
