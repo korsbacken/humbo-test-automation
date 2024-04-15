79b752ab0d2919e28b32f83d3c6202ea39e6d07f
korsbacken <korsbacken@gmail.com>
2024-04-15 22:45:55 +0200

    Update Hooks.java

--- src/test/java/com/humbo/testautomation/helpers/Hooks.java
+++ src/test/java/com/humbo/testautomation/helpers/Hooks.java
@@ -19,30 +19,30 @@ public class Hooks {
     @Before
     public void setup() {
 
-        EdgeOptions options = new EdgeOptions();
+//        EdgeOptions options = new EdgeOptions();
 //        ChromeOptions options = new ChromeOptions();
-//        FirefoxOptions options = new FirefoxOptions();
+        FirefoxOptions options = new FirefoxOptions();
 
 //        options.addArguments("--start-maximized");
 
 //        options.addArguments("--incognito");    //Chrome
 //        options.addArguments("--inPrivate");    //Edge
 
-        options.addArguments("--headless=new");
+//        options.addArguments("--headless=new");
 //        options.addArguments("--window-size=1920,1080");
 
 //        options.addArguments("-private");       //Firefox
 //        options.addArguments("-height=1080");   //Firefox
 //        options.addArguments("-width=1920");    //Firefox
-//        options.addArguments("-headless");      //Firefox
+        options.addArguments("-headless");      //Firefox
 
 //        options.addArguments("--no-sandbox");
 //        options.addArguments("--disable-dev-shm-usage");
 //        options.addArguments("--disable-gpu");
 
 //        driver = new ChromeDriver(options);
-//        driver = new FirefoxDriver(options);
-        driver = new EdgeDriver(options);
+        driver = new FirefoxDriver(options);
+//        driver = new EdgeDriver(options);
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         System.out.println("Browser has opened");
